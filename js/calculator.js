3f09da88edef113f4814b3a96e06f4bfb139cfc7
Dawson Botsford <dawsonbotsford@Dawsons-MacBook-Pro.local>
2015-07-04 00:56:42 -0700

    refactor file locations, added title at top of popup

--- js/calculator.js
+++ js/calculator.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function main() {
-     console.log('calculating difference');
      var previous_value = document.getElementById('previous').value;
      var current_value = document.getElementById('current').value;
      var difference = Math.round(( (current_value - previous_value) / previous_value ) * 100, -2);
